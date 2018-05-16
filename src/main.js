a4e0eaadd6b680e9a7c68f16c8e17ff408901398
simon-zhangmuye <muyezhang@gmail.com>
2018-05-16 22:06:32 +0800

    header

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <home-header></home-header>
+    </div>
+</template>
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  },
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style>
+</style>
