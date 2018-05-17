d7f7ff5b0015937182a2f68e923cfb3e6c7e03f2
simon-zhangmuye <muyezhang@gmail.com>
2018-05-17 19:33:37 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,21 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
