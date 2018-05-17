7b37f08e1b8cd6b176fcd64fafe7bae96fcd9650
simon-zhangmuye <muyezhang@gmail.com>
2018-05-17 08:56:08 +0800

    this is a swiper in index page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
       <home-header></home-header>
+      <home-swiper></home-swiper>
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   data () {
     return {}
   },
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
