f9b66ea281ea42fa8aaaa03024aa87d5878668af
simon-zhangmuye <muyezhang@gmail.com>
2018-05-17 09:14:55 +0800

    this is an icon list in index page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,11 +2,13 @@
     <div>
       <home-header></home-header>
       <home-swiper></home-swiper>
+      <home-icons></home-icons>
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   data () {
@@ -14,7 +16,8 @@ export default {
   },
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
