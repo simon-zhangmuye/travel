62534adefe6944927ab1a0e69078443201530a2b
simon-zhangmuye <muyezhang@gmail.com>
2018-05-17 19:51:59 +0800

    add to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      padding: 0 .1rem
+      height: .62rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
